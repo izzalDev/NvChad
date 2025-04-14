89200552094b219785dd22d801ae796d6aaf2803
Rizal Fadlullah <izzal@Rizals-MacBook-Air.local>
2025-04-14 12:00:03 +0700

    chore: Added plugins and settings "okuuva/auto-save.nvim"

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,26 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0',
+    cmd = "ASToggle",
+    event = { "InsertLeave", "TextChanged" },
+    opts = {
+      enabled = true,
+      trigger_events = {
+        immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" },
+        defer_save = { "InsertLeave", "TextChanged" },
+        cancel_deferred_save = { "InsertEnter" },
+      },
+      condition = nil,
+      write_all_buffers = false,
+      noautocmd = false,
+      lockmarks = false, 
+      debounce_delay = 1000,
+      debug = false,
+    },
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
